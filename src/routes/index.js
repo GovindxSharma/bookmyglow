a7ebbcadf354cedc58a57d11de8d33d12089bd96
GovindxSharma <govindsharma2839@gmail.com>
2025-10-18 11:14:28 +0530

    added payment routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./authRoutes.js";
 import appointmentRoutes from "./appointmentRoutes.js";
 import serviceRoutes from "./serviceRoutes.js";
 import attendanceRoutes from "./attendanceRoutes.js";
+import paymentRoutes from "./paymentRoutes.js";
 
 
 const router = express.Router();
@@ -13,7 +14,7 @@ router.use("/services", serviceRoutes);
 router.use("/attendance", attendanceRoutes);
 
 // router.use("/customers", customerRoutes);
-// router.use("/payments", paymentRoutes);
+router.use("/payments", paymentRoutes);
 // router.use("/inventory", inventoryRoutes);
 
 export default router;
